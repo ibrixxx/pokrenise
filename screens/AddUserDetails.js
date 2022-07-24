0dea1f642e7edb1e086e6173328d7af3958ac0dc
ibrahimmesan <ibrahinmesan@gmail.com>
2022-07-25 00:00:54 +0200

    profile done almost emojis suck

--- screens/AddUserDetails.js
+++ screens/AddUserDetails.js
@@ -9,24 +9,63 @@ import {Entypo} from "@expo/vector-icons";
 import AppIntroSlider from "react-native-app-intro-slider";
 import {auth} from "../firebase";
 import ColorPicker from "react-native-wheel-color-picker";
+import EmojiSelector from "react-native-emoji-selector";
+import { Picker } from 'rn-emoji'
 
 
 const AddUserDetails = ({navigation}) => {
     const theme = 'dark'
-    const [nick, setNick] = React.useState("");
 
     const picker = useRef(null)
 
+    const [nick, setNick] = React.useState("");
+    const [emoji, setEmoji] = React.useState('🧠');
     const [visible, setVisible] = React.useState(false);
+    const [visible2, setVisible2] = React.useState(false);
+    const [selectedColors, setSelectedColors] = React.useState(['#ffa500', '#00ffff']);
+    const [modalColor, setModalColor] = React.useState('#fff');
     const [selectedColor, setSelectedColor] = React.useState(null);
 
-    const showModal = () => setVisible(true);
-    const hideModal = () => setVisible(false);
+    const formatNick = t => {
+        setNick(t)
+    }
+
+    const showModal = i => {
+        if(i < selectedColors.length)
+            setModalColor(selectedColors[i])
+        setSelectedColor(i)
+        setVisible(true)
+    };
+    const hideModal = () => {
+        setVisible(false);
+        setModalColor('#fff')
+    }
+
+    const showModal2 = () => {
+        setVisible2(true)
+    };
+    const hideModal2 = () => {
+        setVisible2(false);
+    }
+
+    const addOrChangeColor = () => {
+        let pom = selectedColors
+        if(selectedColor < selectedColors.length)
+            pom[selectedColor] = modalColor
+        else
+            pom.push(modalColor)
+        setSelectedColors(pom)
+        hideModal()
+    }
+
+    const removeColor = () => {
+        setSelectedColors(selectedColors.filter((c, i) => i !== selectedColor))
+        hideModal()
+    }
 
     const onConfirm = () => {
         auth.signOut().then(() => navigation.replace('ProfileHome')).catch(e => console.log(e))
     }
-
 
     const slides = [
         {
@@ -53,10 +92,13 @@ const AddUserDetails = ({navigation}) => {
                         mode={'outlined'}
                         style={[styles.input, {backgroundColor: Colors[theme].background}]}
                         label="@"
+                        placeholder={'@nabildani_mozak'}
                         value={nick}
                         activeOutlineColor={Colors[theme].primary}
                         outlineColor={Colors[theme].tabIconDefault}
-                        onChangeText={text => setNick(text)}
+                        onChangeText={text => {
+                            formatNick(text)
+                        }}
                         theme={{ colors: { text: Colors[theme].text, placeholder: Colors[theme].tabIconDefault }}}
                     />
                     </View>
@@ -70,22 +112,31 @@ const AddUserDetails = ({navigation}) => {
                     <View style={{width: '100%', alignItems: 'center', flex: 4}}>
                         <View style={styles.profileOutline}>
                             <LinearGradient
-                                colors={['orange', 'aqua']}
+                                colors={selectedColors}
                                 style={styles.profileBackground}
                             >
-                                <Text style={{fontSize: scale(55)}}>🧠</Text>
+                                <Text style={{fontSize: scale(55)}}>{emoji}</Text>
                             </LinearGradient>
                         </View>
                         <View style={{flexDirection: 'row', alignItems: 'center', width: '70%', justifyContent: 'space-between'}}>
-                            <TouchableOpacity onPress={showModal} style={styles.colorOutline}>
-                                <View style={[styles.colorBackground, {backgroundColor: 'orange'}]}/>
-                            </TouchableOpacity>
-                            <TouchableOpacity onPress={showModal} style={styles.colorOutline}>
-                                <View style={[styles.colorBackground, {backgroundColor: 'aqua'}]}/>
-                            </TouchableOpacity>
-                            <TouchableOpacity onPress={showModal} style={styles.colorOutline}>
+                            {
+                                selectedColors.map((c, i) => {
+                                        return (<TouchableOpacity key={i} onPress={() => showModal(i)} style={styles.colorOutline}>
+                                                    <View style={[styles.colorBackground, {backgroundColor: c}]}/>
+                                                </TouchableOpacity>)
+                                })
+                            }
+                            {
+                                selectedColors.length < 3 &&
+                                <TouchableOpacity onPress={() => showModal(selectedColors.length)} style={styles.colorOutline}>
+                                    <View style={styles.colorBackground}>
+                                        <Entypo name="plus" size={33} color="gray"/>
+                                    </View>
+                                </TouchableOpacity>
+                            }
+                            <TouchableOpacity onPress={showModal2} style={styles.colorOutline}>
                                 <View style={styles.colorBackground}>
-                                    <Entypo name="plus" size={33} color="gray" />
+                                    <Text style={{fontWeight: 'bold', fontSize: scale(19)}}>{emoji}</Text>
                                 </View>
                             </TouchableOpacity>
                         </View>
@@ -98,20 +149,20 @@ const AddUserDetails = ({navigation}) => {
                 <View style={{width: '100%', alignItems: 'center', flex: 4}}>
                     <View style={styles.profileOutline}>
                         <LinearGradient
-                            colors={['orange', 'aqua']}
+                            colors={selectedColors}
                             style={styles.profileBackground}
                         >
-                            <Text style={{fontSize: scale(55)}}>🧠</Text>
+                            <Text style={{fontSize: scale(55)}}>{emoji}</Text>
                         </LinearGradient>
                     </View>
-                    <Title style={[styles.title, {color: Colors[theme].text, width: '100%'}]}>@nabildani_mozak</Title>
+                    <Title style={[styles.title, {color: Colors[theme].text, width: '100%', fontSize: scale(27)}]}>@{nick.toLowerCase()}</Title>
                 </View>
             </View>
         )
     }
 
     const nextButton = () => (
-        <Button uppercase={false} style={styles.button} mode="contained" color={Colors[theme].primary}>
+        <Button disabled={nick.length < 2} uppercase={false} style={styles.button} mode="contained" color={Colors[theme].primary}>
             <Text style={[styles.title, {color: Colors[theme].text}]}>aj dalje</Text>
         </Button>
     )
@@ -131,28 +182,32 @@ const AddUserDetails = ({navigation}) => {
 
     return(
         <View style={{width: '100%', height: '100%'}}>
+
             <AppIntroSlider
                 renderItem={renderItem}
                 data={slides}
+                scrollEnabled={false}
                 onDone={onConfirm}
                 showPrevButton={true}
                 renderDoneButton={doneButton}
                 renderNextButton={nextButton}
                 renderPrevButton={prevButton}
+                dotClickEnabled={false}
                 dotStyle={{backgroundColor: Colors[theme].tabIconDefault}}
                 activeDotStyle={{backgroundColor: Colors[theme].primary}}
             />
+
             <Modal
                 visible={visible}
                 onDismiss={hideModal}
                 contentContainerStyle={{paddingHorizontal: scale(20), borderRadius: scale(12), height: '59%', width: '100%', alignItems: 'center', justifyContent: 'space-between', backgroundColor: Colors[theme].background}}>
-                <Text style={[styles.title, {flex: 1}]}>{selectedColor}</Text>
+                <Text style={[styles.title, {flex: 1}]}>{modalColor}</Text>
                 <View style={{flex: 4, marginBottom: verticalScale(30), width: '90%'}}>
                     <ColorPicker
                         ref={picker}
-                        color={selectedColor}
-                        onColorChange={color => setSelectedColor(color)}
-                        onColorChangeComplete={color => setSelectedColor(color)}
+                        color={modalColor}
+                        onColorChange={color => setModalColor(color)}
+                        onColorChangeComplete={color => setModalColor(color)}
                         thumbSize={40}
                         sliderSize={40}
                         noSnap={false}
@@ -162,11 +217,26 @@ const AddUserDetails = ({navigation}) => {
                     />
                 </View>
                 <View style={{flexDirection: 'row', justifyContent: 'space-between',  width: '100%'}}>
-                    <Button uppercase={false} style={styles.button} mode="outlined" color={Colors[theme].primary}><Text style={[styles.title, {color: 'firebrick'}]}>ukloni</Text></Button>
-                    <Button uppercase={false} style={styles.button} mode="outlined" color={Colors[theme].primary}><Text style={[styles.title, {color: Colors[theme].text}]}>otkaži</Text></Button>
-                    <Button uppercase={false} style={styles.button} mode="contained" color={Colors[theme].primary}><Text style={[styles.title, {color: Colors[theme].text}]}>izaberi</Text></Button>
+                    {selectedColors.length === 3 && <Button onPress={removeColor} uppercase={false} style={styles.button} mode="outlined" color={Colors[theme].primary}><Text style={[styles.title, {color: 'firebrick'}]}>ukloni</Text></Button>}
+                    <Button onPress={hideModal} uppercase={false} style={styles.button} mode="outlined" color={Colors[theme].primary}><Text style={[styles.title, {color: Colors[theme].text}]}>otkaži</Text></Button>
+                    <Button onPress={addOrChangeColor} uppercase={false} style={styles.button} mode="contained" color={Colors[theme].primary}><Text style={[styles.title, {color: Colors[theme].text}]}>izaberi</Text></Button>
                 </View>
             </Modal>
+
+            <Modal
+                visible={visible2}
+                onDismiss={hideModal2}
+                contentContainerStyle={{paddingHorizontal: scale(20), borderRadius: scale(12), height: '69%', width: '100%', alignItems: 'center', justifyContent: 'space-between', backgroundColor: Colors[theme].background}}>
+                <Text style={[styles.title, {flex: 1, fontStyle: 'normal'}]}>{emoji}</Text>
+                <View style={{flex: 4, marginBottom: verticalScale(30), width: '90%'}}>
+                    <EmojiSelector onEmojiSelected={emoji => setEmoji(emoji)} theme={Colors[theme].primary} showSearchBar={false}/>
+                </View>
+                <View style={{flexDirection: 'row', justifyContent: 'space-between',  width: '100%', marginTop: verticalScale(20)}}>
+                    <Button onPress={hideModal2} uppercase={false} style={styles.button} mode="outlined" color={Colors[theme].primary}><Text style={[styles.title, {color: Colors[theme].text}]}>otkaži</Text></Button>
+                    <Button onPress={hideModal2} uppercase={false} style={styles.button} mode="contained" color={Colors[theme].primary}><Text style={[styles.title, {color: Colors[theme].text}]}>izaberi</Text></Button>
+                </View>
+            </Modal>
+
         </View>
     )
 }
