dff0b86d63338bf9bbdc0c8bef174f1b8cebd27e
ibrahimmesan <ibrahinmesan@gmail.com>
2022-09-18 00:45:40 +0200

    now playing done

--- components/NowPlaying.js
+++ components/NowPlaying.js
@@ -1,19 +1,21 @@
 import React from "react";
 import {Image, Pressable, StyleSheet, TouchableOpacity} from "react-native";
-import {Text, View} from "./Themed";
+import {View} from "./Themed";
 import { scale } from 'react-native-size-matters'
 import Colors from "../constants/Colors";
-import { LinearGradient } from 'expo-linear-gradient';
 import {AntDesign, Entypo} from "@expo/vector-icons";
 import {useRecoilState, useRecoilValue} from "recoil";
-import {currentAudioInstance, currentStatus} from "../atoms/AudioFunctions";
+import {currentAudioInstance, currentAudioObject, currentPlaylist, currentStatus} from "../atoms/AudioFunctions";
 import {useNavigation} from "@react-navigation/native";
+import TextTicker from "react-native-text-ticker";
 
 const NowPlaying = () => {
     const theme = 'dark'
 
     const [currAudioInstance, setCurrAudioInstance] = useRecoilState(currentAudioInstance)
     const currStatus = useRecoilValue(currentStatus)
+    const currAudioObject = useRecoilValue(currentAudioObject)
+    const currPlaylist = useRecoilValue(currentPlaylist)
     const navigation = useNavigation()
 
     const onPress = () => {
@@ -50,10 +52,24 @@ const NowPlaying = () => {
         }
     }
 
+    const getImageSource = () => {
+        return currPlaylist[currAudioObject]?.imageUrl?  {uri: currPlaylist[currAudioObject]?.imageUrl} : require('../assets/images/motiv.png')
+    }
+
     return (
         <Pressable style={styles.card} onPress={onPress}>
-            <Image source={require('../assets/images/rio-bg.jpeg')} style={{width: scale(55), height: '100%'}} resizeMode={'cover'} />
-            <Text>Naslov</Text>
+            <Image source={getImageSource()} style={{width: scale(55), height: '100%'}} resizeMode={'cover'} />
+            <View style={{width: scale(175), alignItems: 'center', backgroundColor: '#3d3604'}}>
+                <TextTicker
+                    style={{ fontSize: 14, color: 'white', fontWeight: 'bold'}}
+                    useNativeDriver={true}
+                    repeatSpacer={50}
+                    marqueeDelay={2000}
+                    scrollSpeed={24}
+                >
+                    {currPlaylist[currAudioObject]?.title?? currPlaylist[currAudioObject]}
+                </TextTicker>
+            </View>
             <View style={styles.container}>
                 <TouchableOpacity style={{marginRight: scale(10)}} onPress={onToggle}>
                     <Entypo name={currStatus?.isPlaying? "controller-paus":"controller-play"} size={30} color={Colors[theme].primary} />
